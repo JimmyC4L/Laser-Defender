9130fe06bfbfa28a607faf5ca8fd813b8804bf36
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2018-12-09 22:32:06 -0600

    scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [FormerlySerializedAs("backgroundScrollSpeed")] [SerializeField]
+    private float _backgroundScrollSpeed = 0.5f;
+
+    private Material _material;
+    private Vector2 _offSet;
+
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        _offSet = new Vector2(0, _backgroundScrollSpeed);
+    }
+
+    private void Update()
+    {
+        _material.mainTextureOffset += _offSet * Time.deltaTime;
+    }
+}
