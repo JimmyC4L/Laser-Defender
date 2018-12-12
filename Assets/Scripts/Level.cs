b9770f0a01db457292ed0e2ed13e04b4d04d572c
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2018-12-12 00:36:04 -0600

    Added particle effect and sound effects. Start and Game over scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+	public void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+	
+	public void LoadGameScene()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void LoadGameOverScene()
+	{
+		SceneManager.LoadScene("Game Over");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
